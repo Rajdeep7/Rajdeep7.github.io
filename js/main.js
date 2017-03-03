fb5a3cd3dd46c5bb18669b054a5403bac36637b4
Rajdeep Surolia <rajdeep7@icloud.com>
2017-03-03 23:34:33 +0530

    made changes in page load

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@
     	$("#loader").fadeOut("slow", function(){
 
         // will fade out the whole DIV that covers the website.
-        $("#preloader").delay(300).fadeOut("slow");
+        $("#preloader").delay(30).fadeOut("slow");
 
       });       
 
